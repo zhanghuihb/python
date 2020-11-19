df1d2634c2f8c9774838100053bfa187a0e8c0de
zhanghuihb <zhanghuihb@126.com>
2020-11-19 20:01:39 +0800

    里阿尼爬虫

--- python/spider/Spider_dzdp.py
+++ python/spider/Spider_dzdp.py
@@ -8,10 +8,10 @@ entry_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit
            'Referer': 'http://www.dianping.com/shanghai/ch10',
            'Cookie': 'fspop=test; cy=1; cye=shanghai; _lxsdk_cuid=175d8e9c66fc8-0d6334719e6d83-7b10374c-1fa400-175d8e9c66fc8; _lxsdk=175d8e9c66fc8-0d6334719e6d83-7b10374c-1fa400-175d8e9c66fc8; _hc.v=0267b309-3904-cc87-7f6f-06a52aa7e104.1605662001; aburl=1; s_ViewType=10; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1605662008,1605662152,1605663061,1605748733; _lx_utm=utm_source%3Ddp_pc_event; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1605769464; _lxsdk_s=175df1a4171-acc-7b0-99e%7C%7C117'}
 response = requests.get(entry_link, headers=entry_headers, timeout=5)
-soup = BeautifulSoup(response.text, 'html.parser')
-# print(soup.prettify())
-# print(response.status_code)
-# print(response.cookies)
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup.prettify())
+print(soup.title.name)
+print(soup.link['href'])
 # print(soup.find('div',class_='tit').a.text.strip())
 # shopNameDivs = soup.find_all('div',class_='tit')
 # shop_list = []
@@ -21,7 +21,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 # print(shop_list)
 
 # driver = webdriver.Firefox()
-driver = webdriver.Chrome()
-browser = driver.get('http://www.dianping.com/shop/G7lZQSVUguP43EIT')
-comment = driver.find_element_by_css_selector('h1.shop-name')
-print(comment)
+# driver = webdriver.Chrome()
+# browser = driver.get('http://www.dianping.com/shop/G7lZQSVUguP43EIT')
+# comment = driver.find_element_by_css_selector('h1.shop-name')
+# print(comment)
